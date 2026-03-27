cb0ca08de519cd3805b111be6937dd7febbe5ff0
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-03-27 16:39:48 -0600

    feat: update zone controller to apply role-based access for all endpoints

--- src/modules/zones/zones.controller.ts
+++ src/modules/zones/zones.controller.ts
@@ -9,41 +9,47 @@ import { Role } from '@common/enums/role.enum';
 
 @Controller('zones')
 @UseGuards(JwtAuthGuard, RolesGuard)
-@Roles(Role.ADMIN)
 export class ZonesController {
   constructor(private readonly zonesService: ZonesService) { }
 
   @Get()
+  @Roles(Role.ADMIN, Role.CASHIER)
   findAll() {
     return this.zonesService.findAll();
   }
 
   @Get('branch/:branchId')
+  @Roles(Role.ADMIN, Role.CASHIER)
   findByBranch(@Param('branchId', ParseUUIDPipe) branchId: string) {
     return this.zonesService.findByBranchId(branchId);
   }
 
   @Get(':id')
+  @Roles(Role.ADMIN, Role.CASHIER)
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.zonesService.findById(id);
   }
 
   @Post()
+  @Roles(Role.ADMIN)
   create(@Body() createZoneDto: CreateZoneDto) {
     return this.zonesService.create(createZoneDto);
   }
 
   @Patch(':id')
+  @Roles(Role.ADMIN)
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateZoneDto: UpdateZoneDto) {
     return this.zonesService.update(id, updateZoneDto);
   }
 
   @Patch(':id/restore')
+  @Roles(Role.ADMIN)
   restore(@Param('id', ParseUUIDPipe) id: string) {
     return this.zonesService.restore(id);
   }
 
   @Delete(':id')
+  @Roles(Role.ADMIN)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.zonesService.remove(id);
   }
